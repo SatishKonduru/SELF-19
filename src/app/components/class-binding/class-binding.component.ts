ec65d27ddd69cfe0270479ec3e2ae4ab84d6b276
satish <satishkonduru0225@gmail.com>
2025-05-24 09:52:22 +0530

    Class Binding

---
+++ src/app/components/class-binding/class-binding.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-class-binding',
+  imports: [CommonModule],
+  templateUrl: './class-binding.component.html',
+  styleUrl: './class-binding.component.css',
+})
+export class ClassBindingComponent {
+  textColor = 'myTextColor';
+  textSize = 'myTextSize';
+  myGroupClass = {
+    myTextColor: true,
+    myTextSize: true,
+  };
+}

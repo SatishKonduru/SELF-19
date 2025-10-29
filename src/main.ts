db4d0a87ec4a69f8a5ef48788eb0f26a5cdc907c
satish <satishkonduru0225@gmail.com>
2025-10-29 09:42:46 +0530

    @angular/animations included

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, {
+//   ...appConfig,
+//   providers: [
+//     ...(appConfig.providers || []),
+//     provideAnimations(), // ✅ enables Angular Material animations
+//   ],
+// }).catch((err) => console.error(err));

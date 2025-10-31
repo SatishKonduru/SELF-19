26537b5aa8c585ba6d109a9ee14ce48f20467376
satish <satishkonduru0225@gmail.com>
2025-10-31 19:41:27 +0530

    Working on Deferable Views

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CourseDetailsComponent } from '../course-details/course-details.component';
 @Component({
   selector: 'app-home',
-  imports: [RouterModule, MatButtonModule],
+  imports: [
+    RouterModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    CourseDetailsComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  showDetails = signal(false);
+}
